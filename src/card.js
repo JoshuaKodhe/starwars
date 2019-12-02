797d51a88bc1830659011d2a509b313f4eed8ccd
Joshua Kodhe <jkodhe32@gmail.com>
2019-12-02 10:15:36 +0300

    Display basic people from list  - Add card component to display people

---
+++ src/card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Card = ({
+  id,
+  name,
+  height,
+  mass,
+  hairColor,
+  skinColor,
+  eyeColor,
+  birthYear,
+  gender,
+}) => {
+  return (
+    <div className='dib br3 ba pa3 ma3 grow bw1 shadow-5'>
+      <h3>
+        Name: <span>{name}</span>
+      </h3>
+   
+      <span>height: {height}</span>
+      <br />
+      <span>mass: {mass}</span>
+      <br />
+      <span>hair color: {hairColor}</span>
+      <br />
+      <span>skin color: {skinColor}</span>
+      <br />
+      <span>eye color: {eyeColor}</span>
+      <br />
+      <span>birth year: {birthYear}</span>
+      <br />
+      <span>gender: {gender}</span>
+      <br />
+    </div>
+  );
+};
+
+export default Card;
